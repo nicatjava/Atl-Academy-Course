ed89fd968639aeea7241c652ddc036e7c3c54674
Nicat Nabiyev <nicat.nabiyev88@gmail.com>
2024-06-07 17:06:57 +0400

    lesson 9

--- src/lesson9/Main.java
+++ src/lesson9/Main.java
@@ -2,6 +2,7 @@ package lesson9;
 
 public class Main {
     public static void main(String[] args) {
+        // 30 sualdan ibaret imtahan oldu
         //do while ve while kecdik, methodlar haqqinda danisdiq
     }
 }
