f62a8a19b664c15a62ed75737c621106c5bcb110
nicatjava <nicat.nabiyev88@gmail.com>
2024-06-11 14:23:18 +0400

    lesson 10

---
+++ src/lesson10/Main.java
@@ -0,0 +1,16 @@
+package lesson10;
+
+public class Main {
+    public static void main(String[] args) {
+        //=============== ilk 2 saat
+        //String haqqinda genis info
+        // stack ve heap yaddas haqqinda
+        //================ ikinci 2 saat
+        // while loop haqqinda
+
+        String word = "alma";
+        String word2 = new String("alma");
+        String word3 = new String("alma");
+        System.out.println(word2.equals(word3));
+    }
+}
