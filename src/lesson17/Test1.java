ae098c10b0123b6208900666306ebadf558d2b39
nicatjava <nicat.nabiyev88@gmail.com>
2024-06-24 19:45:07 +0400

    lesson 17 with task

--- src/lesson17/Test1.java
+++ src/lesson17/Test1.java
@@ -10,18 +10,31 @@ public class Test1 {
         Scanner sc = new Scanner(System.in);
         System.out.println(randNumber);
 
-        int n= -1;
-        while (n != randNumber)
-        {
+        int n = -1;
+        int count = 0;
+        int[] myArray = new int[count]; // myArray[0]
+        int[] old;
+        while (n != randNumber) {
             System.out.println("Please enter a number: ");
             n = sc.nextInt();
-            if (n == randNumber){ break; }
-            if(n>randNumber){
+            old = myArray;
+            myArray = new int[++count];
+            for (int i = 0; i < old.length; i++) {
+                myArray[i] = old[i];
+            }
+            myArray[myArray.length - 1] = n;
+            if (n == randNumber) {
+                break;
+            }
+            if (n > randNumber) {
                 System.out.println("Your number is too big. Please, try again");
-            }else{
+            } else {
                 System.out.println("Your number is too small. Please, try again.");
             }
         }
         System.out.println("Congratulate! You Find Number!");
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
     }
 }
